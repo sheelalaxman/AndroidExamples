9a23c5cc8a5ec3c9f03c7ea24f8a20a8b18aa961
sheelalaxman <laxman.sheela251@gmail.com>
2018-06-15 01:12:46 +0530

    bug fixed

--- FileExplorer/app/src/main/java/com/example/laxman/fileexplorer/SelectChoice.kt
+++ FileExplorer/app/src/main/java/com/example/laxman/fileexplorer/SelectChoice.kt
@@ -5,12 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ListView
+import android.widget.Spinner
 
 
 class SelectChoice : AppCompatActivity() {
 
-    var mybox :ListView?= null
+    var mybox : Spinner?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selectchoice)
@@ -21,10 +21,10 @@ class SelectChoice : AppCompatActivity() {
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
 
-                var i= Intent(this@SelectChoice,ShowActivity::class.java)
-                i.putExtra("internal1","storage/emulated0/")
+             /*   var i= Intent(this@SelectChoice,ShowActivity::class.java)
+                i.putExtra("internal1","storage/emulated/0/")
                 i.putExtra("internal2","storage/sdcard0/")
-                startActivity(i)
+                startActivity(i)*/
 
 
 
@@ -32,22 +32,21 @@ class SelectChoice : AppCompatActivity() {
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long)
             {
-               if(p2 == 0)
-              {
-                 var i= Intent(this@SelectChoice, ShowActivity::class.java)
-                  i.putExtra("internal1","storage/emulated0/")
-                  i.putExtra("internal2","storage/sdcard0/")
-                  startActivity(i)
-              }
-               if(p2 == 1)
-              {
-                  var i= Intent(this@SelectChoice,ShowActivity::class.java)
-                  i.putExtra("internal1","storage/extsdcard/")
-                  i.putExtra("internal2","storage/sdcard1/")
-                  startActivity(i)
-              }
+                if(p2 > 0) {
+                    if (p2 == 1) {
+                        var i = Intent(this@SelectChoice, ShowActivity::class.java)
+                        i.putExtra("internal1", "storage/emulated/0/")
+                        i.putExtra("internal2", "storage/sdcard0/")
+                        startActivity(i)
+                    }
+                    if (p2 == 2) {
+                        var i = Intent(this@SelectChoice, ShowActivity::class.java)
+                        i.putExtra("internal1", "storage/4E14-9138/")
+                        i.putExtra("internal2", "storage/sdcard1/")
+                        startActivity(i)
+                    }
 
-
+                }
             }
 
         }
--- FileExplorer/app/src/main/java/com/example/laxman/fileexplorer/ShowActivity.kt
+++ FileExplorer/app/src/main/java/com/example/laxman/fileexplorer/ShowActivity.kt
@@ -3,30 +3,32 @@ package com.example.laxman.fileexplorer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.activity_show.*
+import android.widget.ListView
+import android.widget.Toast
 import java.io.File
 
 class ShowActivity : AppCompatActivity() {
 
+    var mylist:ListView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show)
-       // readFiles()
+        readfiles()
     }
 
-   /* private fun readFiles() {
+    fun readfiles() {
         var path=intent.getStringExtra("internal1")
-        var f= File(path)
-
+        var f = File(path)
         if(!f.exists())
         {
             path=intent.getStringExtra("internal2")
-            f= File(path)
+            f = File(path)
         }
-        var values = f.list()
+        var values= f.list()
+        var myadapter = ArrayAdapter<String>(this,R.layout.listcomponent,values)
+        mylist =findViewById<ListView>(R.id.display)
+        mylist?.adapter = myadapter
 
-        var adapter = ArrayAdapter<String>(this,R.layout.listcomponent,values)
-
-        list.adapter = adapter
-    }*/
+        Toast.makeText(this,path,Toast.LENGTH_LONG).show()
+    }
 }
