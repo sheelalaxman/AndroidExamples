101f854641faeebb27c58cbf443a07dde0ae8673
sheelalaxman <laxman.sheela251@gmail.com>
2018-07-14 00:56:10 +0530

    perfectly done

--- cntproviders/app/src/main/java/com/lmndevelopers/cntproviders/MainActivity.kt
+++ cntproviders/app/src/main/java/com/lmndevelopers/cntproviders/MainActivity.kt
@@ -8,6 +8,7 @@ import android.provider.ContactsContract
 import android.view.View
 import android.widget.AdapterView
 import android.widget.SimpleCursorAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,17 +18,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var cr = contentResolver
-        var c=cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,null)
+        var c=cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
         var adapter=SimpleCursorAdapter(this,R.layout.indiview,c, arrayOf(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER),
                 intArrayOf(R.id.name,R.id.number),0)
         lview.adapter = adapter
 
         lview?.onItemClickListener = object : AdapterView.OnItemClickListener {
             override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                var list = ContactsContract.CommonDataKinds.Phone.NUMBER
-               var i= Intent()
-               i.action = Intent.ACTION_CALL
-                i.data = Uri.parse("tel" + list.get(p2) )
+                var phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                var list= mutableListOf<String>()
+                while (phones.moveToNext()) {
+                    var name = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    var phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                    list.add(phoneNumber)
+                }
+               var i = Intent(Intent.ACTION_CALL)
+                //Toast.makeText(this@MainActivity,list.get(p2),Toast.LENGTH_LONG).show()
+                i.data = Uri.parse("tel:${list.get(p2)}")
+                startActivity(i)
 
             }
 
